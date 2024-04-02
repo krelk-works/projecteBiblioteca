ba3677166b529b5ebc6813ea5de1956a9f77dd14
krelk <leo.craciun97@gmail.com>
2024-04-02 20:58:31 +0200

    primera función y switch

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,17 @@
-print('Hola mundo')
+import sys
+    
+def login(username, password):
+    print("Inicio de sesión de : ",username, password)
 
-print ('GUARRA')
+try:
+    match sys.argv[1]:
+        case "-login":
+            try:
+                login(sys.argv[2], sys.argv[3])
+            except IndexError:
+                print("No has ingresado una contraseña o un nombre de usuario, vuelve a intentarlo, si us plau.")
+        case _:
+            print("El parámetro ingresado no es correcto, vuelve a leer las instrucciones del xampú, si us plau.")
+except IndexError:
+    print("No has puesto ningún parámetro, vuelve a intentarlo, si us plau.")
+        
