f321d2b9741717081660c499934f9b08319f3eea
krelk <leo.craciun97@gmail.com>
2024-04-04 19:11:37 +0200

    Actualització del sistema d'inici de sessió

--- biblioteca.py
+++ biblioteca.py
@@ -1,9 +1,9 @@
+
+
 import sys
 from bibliotecaUtils import *
 
-session = False
-sessionName = None
-sessionTry = 0
+
 
 try:
     match sys.argv[1]:
@@ -12,18 +12,7 @@ try:
                 login(sys.argv[2], sys.argv[3])
             except IndexError:
                 print("No has ingresado una contraseña o un nombre de usuario, vuelve a intentarlo, si us plau.")
-        case "-agregar":
-            try:
-                if session:
-                    agregar_libro(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
-                else:
-                    print("Abans de fer cualsevol acció hauras d'iniciar sesió")
-            except IndexError:
-                print("Faltan datos del libro")
         case _:
             print("El parámetro ingresado no es correcto, vuelve a leer las instrucciones del xampú, si us plau.")
 except IndexError:
     print("No has puesto ningún parámetro, vuelve a intentarlo, si us plau.")
-   
-        
-        
--- bibliotecaUtils.py
+++ bibliotecaUtils.py
@@ -1,14 +1,22 @@
 import hashlib
 
+session = False
+sessionName = None
+sessionTry = 0
+
 def login(username, password):
     global session, sessionName, sessionTry
+    password=encriptar(password)
     with open("usuaris.txt") as archivo:
         for linea in archivo:
             username_bbdd, password_bbdd = linea.split("|")
+            username_bbdd = username_bbdd.strip() ### Evitar errores por espacios en blanco en el nombre
+            password_bbdd = password_bbdd.strip() ### Evitar errores por espacios en blanco en la contraseña
             if username_bbdd == username and password_bbdd == password:
                 session = True
                 sessionName = username
                 print("Benvingut "+username)
+                bibliotecaMenu()
                 break
     
     if session != True and sessionTry < 3:
@@ -17,14 +25,39 @@ def login(username, password):
         newusername = str(input("Nom d'usuari: "))
         newpassword = str(input("Contrasenya: "))
         login(newusername, newpassword)
-    else:
+    elif session != True and sessionTry == 3:
         print("Has esgotat el numero d'intents disponibles, prova-ho més tard.")
         
     
 def encriptar(password):
-    return hashlib.md5(password.encode()).hexdigest()
+    return str(hashlib.md5(password.encode()).hexdigest())
 
 def agregar_libro(titol, autor, any_publicacio, genere, ISBN):
     with open("llibres.txt", "a") as archivo:
         archivo.write(f"{titol}|{autor}|{any_publicacio}|{genere}|{ISBN}\n")
     print("Has añadido el libro correctamente, HIJO DE PUTA")
+    
+def bibliotecaMenu():
+    print("\n")
+    print("=========================")
+    print("Menu d'interració de la Biblioteca")
+    print("[1] Veure un llibre")
+    print("[2] Veure tots els llibres")
+    print("[3] Afegir un llibre")
+    print("[4] Eliminar un llibre")
+    print("[5] Editar un llibre")
+    print("[6] Sortir")
+    print("==========================")
+    
+    try:
+        opcio=int(input("Seleccionar opció: "))
+        match opcio:
+            case 1:
+                ###agregar_libro()
+                print("Agregar libro...")
+            case _:
+                print("\nError : Opcio no válida : "+str(opcio))
+                bibliotecaMenu()
+    except ValueError:
+        print("Error : Només es permés fer ús de numeros per seleccionar una opció")
+        bibliotecaMenu()
